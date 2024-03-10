2e0492482ff3a76f6c891b551eafcb7b9bc85607
hamru <goo.hiro.w@gmail.com>
2024-03-10 18:37:32 +0900

    atomic button component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+
 export const App = () => {
   return (
     <div>
-      <h1>Hello!</h1>
+      <PrimaryButton>open</PrimaryButton>
+      <br />
+      <SecondaryButton>closed</SecondaryButton>
     </div>
   );
 };
---
+++ src/components/atoms/button/BaseButton.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+    color: white;
+    padding: 8px 32px;
+    border: none;
+    border-radius: 16px;
+    outline: nome;
+    &:hover {
+        cursor: pointer;
+        opacity: 0.75;
+    }
+`;
